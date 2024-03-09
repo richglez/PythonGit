f939eb6028976f46d404c94dc7fc43eace814ec8
Rich <rickygb082002@gmail.com>
2024-03-09 11:44:24 -0600

    coment

--- NegociosElectronicos/lecturaExcel.py
+++ NegociosElectronicos/lecturaExcel.py
@@ -1,24 +1,39 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-#Lectura de la base de datos
-df = pd.read_csv('./Excels/diabetes.csv')
+# * NOMBRE: RICARDO GONZALEZ BECERRA
+# * ACTIVIDAD 4. USO DE LIBRERÍA PANDAS
 
-# print(df.head())
 
+
+# * Lectura de la base de datos
+df = pd.read_csv('C:/')
+
+
+# * Impresion del encabezado y primeros registros
+print(df.head())
+
+
+# * Impresion de ultimos registros
 # print(df.tall())
 
+
+# * Impresion de tendencia central
 # print(df.describe())
 
+
+# * Contra todos los registros
 # print(df.count())
 
+
+# * Agrupar y contar los valores de una columna
 # print(df.groupby(['Outcome']).count())
 
 
-#Querires
+# ? -----------Querires------------
 
-#Query 1
-#con diabetes y con familiares con diabetes
+# ? Query 1
+# * con diabetes y con familiares con diabetes
 # diseasePedigree = (df.query('DiabetesPedigreeFunction >= 1 and Outcome>0'))
 
 # print('Enfermos de diabetes con historial familiar de enfermedad', diseasePedigree.loc[:, 'Outcome'].count())
@@ -27,18 +42,18 @@ df = pd.read_csv('./Excels/diabetes.csv')
 
 
 
-#Query 2
+# ? Query 2
 #>= significa que tiene parientes
 #Outcome == 0 no tiene diabetes
 #Outcome > 0 tiene diabetes
 #sin diabetes, sin hijos o con familiares con diabetes
-diseasePedigreeOrChild = (df.query('(DiabetesPedigreeFunction >= 1 or Pregnancies > 0) and Outcome>=0'))
-print('Sin enfermedad con historial familiar de enfermedad', diseasePedigreeOrChild.loc[:, 'Outcome'])
+# diseasePedigreeOrChild = (df.query('(DiabetesPedigreeFunction >= 1 or Pregnancies > 0) and Outcome>=0'))
+# print('Sin enfermedad con historial familiar de enfermedad', diseasePedigreeOrChild.loc[:, 'Outcome'])
 
 
 
 
-#Query 3
+# ? Query 3
 
 #funcion loc
 #otros 2 queries, inventarse otras dos vairables
