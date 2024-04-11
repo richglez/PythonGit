426619454bf4fc41987ed3f28d1763fbafae7436
Rich <rickygb082002@gmail.com>
2024-04-11 16:32:26 -0600

    limpieza de datos

--- NegociosElectronicos/moviesDB.py
+++ NegociosElectronicos/moviesDB.py
@@ -1,3 +1,9 @@
+#
+# todo --Ricardo Gonzalez Becerra
+# todo --Matricula 100171170
+# todo --ACTIVIDAD 6. TÉCNICAS BÁSICAS DE LIMPIEZA DE DATO
+# todo --Fecha 11/04/2024
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -5,56 +11,177 @@ import ast
 import plotly
 import plotly.offline as py 
 
-#Lectura de la base de datos
+
+
+
+
+
+
+#todotodo Significado de las columnas:
+#todo budget: Presupuesto de la película.
+#todo genres: Géneros de la película.
+#todo id: ID único de la película.
+#todo original_language: Idioma original de la película.
+#todo overview: Resumen de la trama de la película.
+#todo popularity: Popularidad de la película.
+#todo production_companies: Compañías de producción de la película.
+#todo release_date: Fecha de lanzamiento de la película.
+#todo revenue: Ingresos generados por la película.
+#todo runtime: Duración de la película en minutos.
+#todo title: Título de la película.
+#todo vote_average: Promedio de votos de la película.
+#todo vote_count: Cantidad de votos de la película.
+#todo adult: Indica si la película es para adultos o no.
+#todo belongs_to_collection: Información sobre a qué colección pertenece la película.
+
+
+
+
+
+#? 1.- Utiliza la base de datos movies_metadata 
 df = pd.read_csv('./Excels/movies_metadata.csv', low_memory=False)
 
 
-#Para conocer todos los datos
-print(df.head(1).transpose())
-print("*"*25)
-print(df.info())
-print(df.describe())
+#? Query 2.1- Utiliza la función transpose() junto con head() para que  conozcas los tres primeros registros del dataset 
+print("\n\n\n\n\n"+"*"*105)
+print("2.1- Utiliza la función transpose() junto con head() para que  conozcas los tres primeros registros del dataset ")
+print("*"*105)
+print(df.head(3).transpose())  #transponer el encabezado 1
+print("*"*105)
 
-#Estado inicial del dataset
+
+
+#? Query 2.2- Conteo de las columnas y de los registros guardados 
+print("\n\n\n\n\n"+"*"*105)
+# print(df.info())  #de que tamano es y de que tipo de datos es
+# print(df.describe())  #describir la tabla de la base de datos
+print("2.2- Conteo de las columnas y de los registros guardados ")
+print(df.shape)  #conteo de las columnas y de los registros guardados
+print("*"*105)
+
+
+
+
+
+
+
+#? Query 3.- Repite el punto dos con la función tail() para conocer los 3 últimos registros del dataset 
+print("\n\n\n\n\n"+"*"*105)
+print("3.- Repite el punto dos con la función tail() para conocer los 3 últimos registros del dataset ")
+print(df.tail(3))
+print("*"*105)
+
+
+
+
+
+
+#? Query Estado inicial del dataset
+print("\n\n\n\n\n"+"*"*105)
+print("Estado inicial del dataset")
 print(df.shape)
+print("*"*105)
 
-#Query de imprimir todos los Campos de lenguaje original en ingles -> shape
+
+
+
+
+
+
+
+
+#? Query: 6.-¿Cuántas películas tienen como idioma original el inglés? 
+print("\n\n\n\n"+"*"*105)
+print("6.- ¿Cuántas películas tienen como idioma original el inglés? ")
 print(df[df['original_language'] == 'en'].shape) 
+print("*"*105)
 
-#Query de impirmir todos los datos en true de la columna adult
-print(df[df['adult'] == 'True'].shape) 
 
-#Query: ¿Cuántas películas NO tienen como idioma original el inglés? 
+
+
+
+
+#? Query: 6.-¿Cuántas películas NO tienen como idioma original el inglés? 
+print("\n\n\n\n"+"*"*105)
+print("6.- ¿Cuántas películas NO tienen como idioma original el inglés? ")
 print(df[df['original_language'] != 'en'].shape) 
+print("*"*105)
+
+
+
+
+
+
+
+#? Query de impirmir todos los datos en true de la columna adult
+print("\n\n\n\n"+"*"*105)
+print("Imprimir todos los datos en true de la columna adult")
+print(df[df['adult'] == 'True'].shape) 
+print("*"*105)
+
+
+
+
+
+
+
+
+
+
 
 
 #------------------------------------------------------------------------------------------------------------------------
-# PREGUNTA 5
+#? Query 5
+print("\n\n\n\n\n"+"*"*105)
+print("¿Es necesario tener el campo original_title? Depura el set de datos para eliminar este campo ")
+print("*"*105)
 print(df.head(6).transpose())
+print("*"*105)
 
-# Como borrar columnas
-df = df.drop('imdb_id', axis=1)
-print("El numero de columnas que resultan despues de eliminar imdb_id", df.shape)
+
+
+
+
+#? Como borrar columnas
+# print("\n\n\n\nBorrar columnas")
+# print("*"*105)
+# df = df.drop('imdb_id', axis=1)
+# print("El numero de columnas que resultan despues de eliminar imdb_id", df.shape)
+# print("*"*105)
 #------------------------------------------------------------------------------------------------------------------------
 
 
-#------------------------------------------------------------------------------------------------------------------------
-# PREGUNTA 6: ¿Cuántas películas NO tienen como idioma original el inglés? 
-# print("El total de los registros que tienen diferentes titulo al idioma ingles: ", df[df['original_title'] != df['title']] [['tile', 'original_title']].shape)
 
-# print("Esta es la lista  delas primeras 5 peliculas cuyo titulo es diferente al ingles:")
-# print(df[df['original_title'] != df['title']][['title','original_title']].head())
-#------------------------------------------------------------------------------------------------------------------------
 
 
 
 #------------------------------------------------------------------------------------------------------------------------
-# PREGUNTA 7: ¿Cuántas películas están identificadas como para adultos? Es necesario tener esta columna 
+#? Query 6 ¿Cuántas películas NO tienen como idioma original el inglés? 
+print("\n\n\n\n6.- ¿Cuántas películas NO tienen como idioma original el inglés? ")
+print("*"*105)
+print("El total de los registros que tienen diferentes titulo al idioma ingles: ", df[df['original_title'] != df['title']][['title', 'original_title']].shape)
+
+print("Esta es la lista  delas primeras 5 peliculas cuyo titulo es diferente al ingles:")
+print(df[df['original_title'] != df['title']][['title','original_title']].head())
+print("*"*105)
+#------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+#------------------------------------------------------------------------------------------------------------------------
+#? Query 7 ¿Cuántas películas están identificadas como para adultos? Es necesario tener esta columna 
 #Numero de datos
-print("\nEl total de los registros que son para adultos: ", df[df['adult'] != 'False'] [['title', 'adult']].shape) 
+print("\n\n\n\n\n"+"*"*105)
+print("El total de los registros que son para adultos: ", df[df['adult'] != 'False'] [['title', 'adult']].shape) 
+print("*"*105)
 
 #Ver los datos
+print("\n\n\n\n\n"+"*"*105)
 print("\nImpresion de los registros que son para adultos: ", df[df['adult'] != 'False'] [['title', 'adult']])
+print("*"*105)
 
 
 #Borrar los registros de las peliculas para adultos 
@@ -65,6 +192,9 @@ print("\n\n\nImpresion de los registros que son para adultos, ahora son: ", df[d
 #Ver los datos
 print("\nImpresion de los registros que son para adultos: ", df[df['adult'] != 'False'] [['title', 'adult']])
 #------------------------------------------------------------------------------------------------------------------------
+
+
+
 
 #sustituyo los 0s del dampo revenue y del campo budget por not number 
 #?cuantos campos tienen el revenue = 07?
@@ -80,6 +210,26 @@ df['budget'] = pd.to_numeric(df['budget'], errors='coerce')
 df['budget'] = df['budget'].replace(0, np.nan)
 df[df['budget'].isnull()].shape
 print("El total de registros con budget = 0 ", df[df['budget'] == 0 ].shape)
+
+
+
+
+
+# 1. Calcular el retorno de inversión para las películas que tienen valores válidos de revenue y budget
+df['return'] = df['revenue'] / df['budget']
+print("Número de películas con retorno de inversión calculado:", df['return'].notnull().sum())
+
+# 2. Crear una tabla resumen
+# a. País de origen de la mayoría de las películas
+print("País de origen de la mayoría de las películas:")
+print(df['production_countries'].explode().value_counts().head(1))
+
+# b. Franquicias con mayor número de películas
+franchise_counts = df['belongs_to_collection'].dropna().apply(lambda x: ast.literal_eval(x)['name']).value_counts()
+print("Franquicias con mayor número de películas:")
+print(franchise_counts.head())
+
+
 
 
 
@@ -108,7 +258,7 @@ df['revenue'] = df['revenue'].replace(np.nan, 0)
 df_fran = df[df['belongs_to_collection'].notnull()]
 
 #Funcion zip para crear una lista iterable resultado de la comparacion de dos objetos 
-for element in zip(df_fran['belongs_to_colletion'], df_fran['revenue']):
+for element in zip(df_fran['belongs_to_collection'], df_fran['revenue']):
     reduced_collection.append(
         {
             'id' : ast.literal_eval(element[0])['id'], #evalua un string y lo parte
